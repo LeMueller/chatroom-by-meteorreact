443be3ce38ce8f368ab73f96bb25d3fb1ea0eb98
lezahng <lezhang@techfak.uni-bielefeld.de>
2017-11-09 10:01:31 +0100

    add collection for keys

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/messagesserver.js';
+import '../imports/api/privateKeys.js';
+import '../imports/api/publicKeys.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
